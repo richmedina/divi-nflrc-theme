6180b508daf0f5e872990a5ae9a1e226ff726db2
Richard Medina <rmedina@hawaii.edu>
2020-06-30 16:42:14 -1000

    custom query time

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@ function dp_dfg_custom_query_function($query, $props) {
     if (isset($props['admin_label']) && $props['admin_label'] === 'Collaborator Grid') {
         return array(
             'post_type' => 'contact',
-            'posts_per_page' => '2',
-            'nflrc_staff' => 0,
+            'posts_per_page' => '5',
+            'nflrc_staff' => 1,
         );
     } 
     // else {
