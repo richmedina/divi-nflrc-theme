3507b6552c69b9ffdd678911b3427d0fdf40101c
Richard Medina <rmedina@hawaii.edu>
2020-06-30 16:27:44 -1000

    custom query time

--- functions.php
+++ functions.php
@@ -6,3 +6,18 @@ add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
  
  
 //you can add custom functions below this line:
+function dp_dfg_custom_query_function($query, $props) {
+    if (isset($props['admin_label']) && $props['admin_label'] === 'Collaborator Grid') {
+        return array(
+            'post_type' => 'project',
+            'posts_per_page' => '12',
+        );
+    } 
+    // else {
+    //     return array(
+    //         'post_type' => 'post',
+    //         'posts_per_page' => '12',
+    //     );
+    // }
+}
+add_filter('dpdfg_custom_query_args', 'dp_dfg_custom_query_function', 10, 2);
