13390d398a8dcc9d2af6aa140d8e98e1f3bbbfe9
Richard Medina <rmedina@hawaii.edu>
2020-10-05 12:10:06 -1000

    debugging tax,term script

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -262,7 +262,7 @@ function nflrc_contact_grid_func($atts, $content = null) {
 
 add_shortcode('nflrc_debug', 'nflrc_debug_func');
 function nflrc_debug_func() {
-		$t = '2010-2014';
+		$t = '2014-2018';
 		$args = array(
 		    'post_type' => array('project', 'prodev', 'publication', 'contact', 'story'),
 		    'tag' => $t,
