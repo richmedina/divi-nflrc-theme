2dde02e1db5deb5b855ea4d7231159d00bdc2f45
Richard Medina <rmedina@hawaii.edu>
2020-10-13 10:55:58 -1000

    category-tag debug

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -298,7 +298,7 @@ function nflrc_debug_func() {
 		        // $post->nflrc_staff = false;
 		        // $is_staff = $post->nflrc_staff;
 
-		        // wp_set_post_categories( $post->ID, 155, true );
+		        wp_set_post_categories( $post->ID, 155, true );
 		        
 		        $debugstr .= "<div>{$post_type} | {$category} | {$title} | {$p_id} | {$terms} | {}</div>";
 
