6608071fb0017dccfb8efe912a43ee0b9f95373f
Richard Medina <rmedina@hawaii.edu>
2020-10-27 12:16:59 -1000

    item view shortcode dev

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -289,7 +289,7 @@ function nflrc_meta_display_func() {
 		
 		$html .= "<p><ul>";
 		if ($d['oer']) $html .= "<li><img src='/media/img/logos/oer-logo.png' alt='OER logo'></li> ";
-		if ($d['url']) $html .= "<li><button class='buttoned'><a href='{$d['url']}' target='_blank'>Access <i class="fas fa-external-link-alt"></i></a></button></li>";
+		if ($d['url']) $html .= "<li><button class='buttoned'><a href='{$d['url']}' target='_blank'>Access <i class='fas fa-external-link-alt'></i></a></button></li>";
 		if ($d['oclc_url']) $html .= "<li><a href='{$d['oclc_url']}' target='_blank'><img src='/media/img/logos/oclc-logo.png' alt='OCLC link'></a></li> ";	
 		if ($d['order_url']) $html .= "<li><button class='buttoned'><a href='{$d['order_url']}' target='_blank'>order</a></button></li> ";
 		if ($d['ext_url']) $html .= "<li><button class='buttoned'><a href='{$d['ext_url']}' target='_blank'>View</a></button></li> ";
