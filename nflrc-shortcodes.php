20e739cd77c23ca2053351c408588cec2cdc29de
Richard Medina <rmedina@hawaii.edu>
2020-10-27 12:14:08 -1000

    item view shortcode dev

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -289,7 +289,7 @@ function nflrc_meta_display_func() {
 		
 		$html .= "<p><ul>";
 		if ($d['oer']) $html .= "<li><img src='/media/img/logos/oer-logo.png' alt='OER logo'></li> ";
-		if ($d['url']) $html .= "<li><a href='{$d['url']}' target='_blank'><button class='buttoned'>Access <i class='fas fa-external-link-alt'></button></a></li>";
+		if ($d['url']) $html .= "<li><a href='{$d['url']}' target='_blank'><button class='buttoned'>Access </button></a></li>";
 		if ($d['oclc_url']) $html .= "<li><a href='{$d['oclc_url']}' target='_blank'><img src='/media/img/logos/oclc-logo.png' alt='OCLC link'></a></li> ";	
 		if ($d['order_url']) $html .= "<li><a href='{$d['order_url']}' target='_blank'><button class='buttoned'>order</button></a></li> ";
 		if ($d['ext_url']) $html .= "<li><a href='{$d['ext_url']}' target='_blank'><button class='buttoned'>View</button></a></li> ";
