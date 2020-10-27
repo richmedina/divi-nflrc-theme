ac4e3c0b81d801dd5d1e076b9e20fccf490f6e64
Richard Medina <rmedina@hawaii.edu>
2020-10-27 12:27:24 -1000

    item view shortcode dev

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -289,8 +289,9 @@ function nflrc_meta_display_func() {
 		if ($resource_type) $html .= "<p>Type: {$resource_type}</p>";
 		
 		$html .= "<p><ul>";
-		if ($d['oer']) $html .= "<li><img src='/media/img/logos/oer-logo.png' alt='OER logo'></li> ";
 		if ($d['url']) $html .= "<li><button class='buttoned'><a href='{$d['url']}' target='_blank'>Access <i class='fas fa-external-link-alt'></i></a></button></li>";
+		if ($d['oer']) $html .= "<li><img src='/media/img/logos/oer-logo.png' alt='OER logo'></li> ";
+		
 		if ($d['oclc_url']) $html .= "<li><a href='{$d['oclc_url']}' target='_blank'><img src='/media/img/logos/oclc-logo.png' alt='OCLC link'></a></li> ";	
 		if ($d['order_url']) $html .= "<li><button class='buttoned'><a href='{$d['order_url']}' target='_blank'>order <i class='fas fa-external-link-alt'></i></a></button></li> ";
 		if ($d['ext_url']) $html .= "<li><button class='buttoned'><a href='{$d['ext_url']}' target='_blank'>External URL <i class='fas fa-external-link-alt'></i></a></button></li> ";
