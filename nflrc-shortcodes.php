08ec44611749ea46bb301084603451117e1b7900
Richard Medina <rmedina@hawaii.edu>
2020-10-26 16:18:21 -1000

    item view shortcode dev

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -275,10 +275,10 @@ function nflrc_meta_display_func() {
 		$html .= "";
 
 	} else if ($d['post_type'] === 'publication') {
-		$html .= "<div>{$d['author']}</div>";
+		$html .= "<p>{$d['author']}</p>";
 		if ($d['oer']) $html .= "<span><img src='/media/img/logos/oer-logo.png' alt='OER logo'></span>";
 		if ($d['oclc_url']) $html .= " <span><a href='{$d['oclc_url']}' target='_blank'><img src='/media/img/logos/oclc-logo.png' alt='OCLC link'></a></span> ";
-		if ($d['order_url']) $html .= " <span class=''><a href='{$d['order_url']}' target='_blank'>order</a></span> ";
+		if ($d['order_url']) $html .= " <span class=''><a href='{$d['order_url']}' target='_blank'><button>order</button></a></span> ";
 		if ($d['ext_url']) $html .= " <span><a href='{$d['ext_url']}' target='_blank'>view</a></span> ";
 
 	} else if ($d['post_type'] === 'prodev') {
