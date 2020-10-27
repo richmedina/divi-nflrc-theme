8655964ecbec7fb66479d9d5980263ea0fd3b6c2
Richard Medina <rmedina@hawaii.edu>
2020-10-26 14:11:46 -1000

    item view shortcode dev

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -278,7 +278,7 @@ function nflrc_meta_display_func() {
 		$html .= "<div>{$d['author']}</div>";
 		if ($d['oer']) $html .= "<span><img src='/media/img/logos/oer-logo.png' alt='OER logo'></span>";
 		if ($d['oclc_url']) $html .= " <span><a href='{$d['oclc_url']}' target='_blank'><img src='/media/img/logos/oclc-logo.png' alt='OCLC link'></a></span> ";
-		if ($d['order_url']) $html .= " <span><a href='{$d['order_url']}' target='_blank'>order</a></span> ";
+		if ($d['order_url']) $html .= " <span class='buttoned'><a href='{$d['order_url']}' target='_blank'>order</a></span> ";
 		if ($d['ext_url']) $html .= " <span><a href='{$d['ext_url']}' target='_blank'>view</a></span> ";
 
 	} else if ($d['post_type'] === 'prodev') {
