4acd417d4e4dfa8064d8517ecf4905f12f7c336d
Richard Medina <rmedina@hawaii.edu>
2020-11-25 12:06:07 -1000

    grid custom query for cat pages

--- functions.php
+++ functions.php
@@ -43,10 +43,12 @@ function dp_dfg_custom_query_function($query, $props) {
     else if (isset($props['admin_label']) && $props['admin_label'] === 'NFLRC Items') {
         // var_dump(get_post());
         $page_cat = $props['module_id'];  // Use the string in CSS ID for category filter
+        var_dump($page_cat);
+
         return array(
             'post_type'     => array('project','publication','prodev','contact','story'),
             'category_name' => $page_cat,
-            'meta_key'      => 'order',
+            'meta_key'      => 'menu_order',
             'orderby'       => 'meta_value_num',
             'order'         => 'DESC',
             'posts_per_page' => 6,            
