e62c1e75a0fd71a7dd20d89aaf91170c54c15c8e
Richard Medina <rmedina@hawaii.edu>
2020-11-25 13:59:44 -1000

    grid custom query for cat pages

--- functions.php
+++ functions.php
@@ -46,7 +46,7 @@ function dp_dfg_custom_query_function($query, $props) {
             'post_type'     => array('project','publication','prodev','contact','story'),
             'category_name' => $page_cat,
             'tag__not_in'   => array( 146 ), // Exclude any post tagged as featured
-            'orderby'       => 'date',
+            'orderby'       => 'menu_order date',
             'order'         => 'DESC',
             'posts_per_page' => -1,            
         );
