6d7edbfc23ae2a2d397b7ba916ee62b71ee3ed3a
Richard Medina <rmedina@hawaii.edu>
2020-11-25 15:22:02 -1000

    grid custom query for cat pages

--- functions.php
+++ functions.php
@@ -81,8 +81,8 @@ function dpdfg_after_read_more($content, $props) {
         // $html .=        wp_trim_words(get_the_excerpt($p->ID), 20, ' ...');  
         // $html .=    "</div>"; 
         $html .=    "<div class='tags'>";
-        $html .=        "<p>" . get_the_term_list($p->ID, 'focus_area', ' ', ' ') . "</p>";
-        $html .=        "<p>{$p_type}</p>";
+        $html .=        "<p>" . get_the_term_list($p->ID, 'focus_area', '<span>', ' ', '</span>') . "</p>";
+        // $html .=        "<p>{$p_type}</p>";
         $html .=    "</div>";
         // $html .= "</div></div>"; 
 
