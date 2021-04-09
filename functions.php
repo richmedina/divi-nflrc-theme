6614218d62fc469ea5ae6cf8f89f3e9e92f8e3b9
Richard Medina <rmedina@hawaii.edu>
2021-04-09 13:05:23 -1000

    adjusting query ordering - debugging

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ function dp_dfg_custom_query_function($query, $props) {
             'post_type'     => array('project','publication','prodev','contact','story'),
             'category_name' => $page_cat,
             // 'tag__not_in'   => array( 146 ), // Exclude any post tagged as featured
-            'orderby'       => array('menu_order' => 'ASC', 'date' => 'DESC'),
+            'orderby'       => array( 'date' => 'DESC'),
             // 'order'         => 'DESC',
             'posts_per_page' => -1,            
         );
